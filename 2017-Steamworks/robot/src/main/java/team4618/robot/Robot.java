7157a048871e5042e1b5351a6e3e344d7efd7320
Helios41 <jonny41@msn.com>
2017-12-21 21:13:50 -0500

    Second session - Actually Drives :)

--- 2017-Steamworks/robot/src/main/java/team4618/robot/Robot.java
+++ 2017-Steamworks/robot/src/main/java/team4618/robot/Robot.java
@@ -1,23 +1,30 @@
 package team4618.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.*;
 
 public class Robot extends SampleRobot {
 
-    RobotDrive drivetrain;
-    Joystick driver;
+    public RobotDrive drivetrain;
+    public Joystick driver;
+    public DoubleSolenoid shifter;
 
     public void robotInit() {
-        drivetrain = new RobotDrive(1, 2, 3, 4);
+        drivetrain = new RobotDrive(1, 2, 0, 3);
+
         driver = new Joystick(0);
+        shifter = new DoubleSolenoid(0, 1);
     }
 
     public void operatorControl() {
         while (isOperatorControl() && isEnabled()) {
-            drivetrain.arcadeDrive(driver.getRawAxis(0) /*power*/, driver.getRawAxis(1) /*rotate*/);
+            drivetrain.arcadeDrive(-driver.getRawAxis(1), driver.getRawAxis(4));
+
+            if(driver.getRawButton(5)) {
+                shifter.set(DoubleSolenoid.Value.kForward);
+            } else {
+                shifter.set(DoubleSolenoid.Value.kReverse);
+            }
+
             Timer.delay(0.05);
         }
     }
